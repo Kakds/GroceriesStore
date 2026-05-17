fd2522eddf7b1deddfb9f1faf690100786bae485
Kakds <252692@astanait.edu.kz>
2026-05-17 19:24:58 +0500

    Update test_cart.py

--- test/test_cart.py
+++ test/test_cart.py
@@ -5,17 +5,17 @@ from models.product import Product
 class TestCart(unittest.TestCase):
     def test_add_item_to_cart(self):
         cart = Cart()
-        apple = Product("1", "Apple", 1.5)
-
-        cart.add_item(apple)
-
-        self.assertTrue(len(cart.items) == 1)
-        self.assertEqual(cart.items[0].name, "Apple")
+        apple = Product("1", "Apple", 1.5, 10, "Fruits")
+        cart.add_item(apple, 1)
+        self.assertEqual(len(cart.items), 1)
+        self.assertEqual(cart.items[0]["product"].name, "Apple")
 
     def test_cart_total(self):
         cart = Cart()
-        cart.add_item(Product("1", "Apple", 1.5))
-        cart.add_item(Product("2", "Bread", 2.0))
-        cart.add_item(Product("3", "Milk", 3.5))
+        cart.add_item(Product("1", "Apple", 1.5, 10, "Fruits"), 1)
+        cart.add_item(Product("2", "Bread", 2.0, 10, "Bakery"), 1)
+        cart.add_item(Product("3", "Milk", 3.5, 10, "Dairy"), 1)
+        self.assertEqual(cart.calculate_total(), 7.0)
 
-        self.assertEqual(cart.total(), 7.0)
+if __name__ == "__main__":
+    unittest.main()
