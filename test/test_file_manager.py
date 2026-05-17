6192ada50fd667308606f5896abb970613e577e0
Kakds <252692@astanait.edu.kz>
2026-05-17 19:28:58 +0500

    Update test_file_manager.py

--- test/test_file_manager.py
+++ test/test_file_manager.py
@@ -5,3 +5,6 @@ class TestValidators(unittest.TestCase):
     def test_email_regex(self):
         self.assertTrue(validate_email("test@example.com"))
         self.assertFalse(validate_email("invalid-email"))
+
+if __name__ == "__main__":
+    unittest.main()
