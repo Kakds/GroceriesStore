a795142999d109df13944cb6caf3fbbca0635eb7
Kakds <252692@astanait.edu.kz>
2026-05-17 19:37:24 +0500

    Update ExpiringProducts.py

--- model/ExpiringProducts.py
+++ model/ExpiringProducts.py
@@ -1,19 +1,22 @@
-from model.Product import Product
+from model.product import Product
+
 
 class ExpiringProducts(Product):
-    def __init__(self, name, product_id, quantity, price, category, expiration_date):
-        super().__init__(name, product_id, category, price, quantity)
+    def __init__(self, product_id, name, price, quantity, category, expiration_date):
+        super().__init__(product_id, name, price, quantity, category)
         self.expiration_date = expiration_date
 
     def is_expired(self, today):
         return self.expiration_date < today
 
     def to_dict(self):
-        data=super().to_dict()
+        data = super().to_dict()
         data["expiration_date"] = self.expiration_date
         data["type"] = "ExpiringProducts"
         return data
 
     def __str__(self):
-        return (f"{super().info()},"
-                f"Expiration date: {self.expiration_date},")
+        return (
+            super().__str__() +
+            f"\nExpiration date: {self.expiration_date}"
+        )
