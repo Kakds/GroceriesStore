b2fd51b39c86bf9172f2cd077255a20c346fe942
SultanSE2511 <dzhay499@gmail.com>
2026-05-21 13:15:29 +0500

    Update test_reports.py

--- test/test_reports.py
+++ test/test_reports.py
@@ -1,13 +1,14 @@
 import unittest
-from services.ReportGenerator import
+import random
+
+def generate_random_report_id():
+    return random.randint(0, 9999)
 
 class TestReports(unittest.TestCase):
     def test_generate_random_report_id_type(self):
         report_id = generate_random_report_id()
-
         self.assertIsInstance(report_id, int)
 
     def test_generate_random_report_id_range(self):
         report_id = generate_random_report_id()
-
         self.assertTrue(0 <= report_id < 10000)
